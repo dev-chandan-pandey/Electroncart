b40d831c58e186024bd568b966ef1ba8140def4d
dev-chandan-pandey <iamchandanpandey11@gmail.com>
2025-10-27 21:11:42 +0530

    backend url added

--- frontend/src/context/AppState.jsx
+++ frontend/src/context/AppState.jsx
@@ -5,11 +5,9 @@ import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const AppState = (props) => {
-   const url = "http://localhost:1000/api";
-
+//  const url = "http://localhost:1000/api";
+ const url = "https://electroncart.onrender.com";
   
-
-
   const [products, setProducts] = useState([]);
   const [token, setToken] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
